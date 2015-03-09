242c4700cf3cfe5e4fe8e4641d72946ea69e31ed
denezra <den.segunial@gmail.com>
2015-03-10 01:16:23 +0800

    Modification

--- Preview Library/development/htmls/js/message-receive.js
+++ Preview Library/development/htmls/js/message-receive.js
@@ -2,5 +2,5 @@ window.addEventListener('message', function(e) {
 	// Check to make sure that this message came from the correct domain.
 	console.log(e.origin);
 	// Update the div element to display the message.
-	console.log(e.data);
+	$( "<div><p>"+ e.data +"</p></div>" ).appendTo( "body" )
 }, false)
